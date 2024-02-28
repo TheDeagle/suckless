46c8cf53b5e5656d8d055afff2eb0da61844cf9c
Haitham Ben bouchta <hben-bou@c1r8p8.1337.ma>
2024-02-28 18:47:05 +0100

    suckless

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,10 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"Cpu: ", "top, -bn 1 | \"grep \"Cpu(s)\" | awk \'{print 100 - $8\"%\"}' ",  5,              0},
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
-	{"Cpu:", "top, -bn 1 | \"grep \"Cpu(s)\" | awk \'{print 100 - $8\"%\"}' ",  5,              0},
+	{": ", "date | awk \'{print $1\" \"$2\" \"$3}\'",					30,		0},
+	{": ", "date | awk \'{print $4}"}
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
